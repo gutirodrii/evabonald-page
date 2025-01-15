8be7d996c4365e459b6e03e1a964b9dc57a9bba0
gutirodrii <gutirodriidev@gmail.com>
2025-01-15 19:07:52 +0100

    deleted instagram carrousel homepage. Section 2 empty. Deleted .env unused

--- src/app/components/Section_two/Section_two.tsx
+++ src/app/components/Section_two/Section_two.tsx
@@ -1,12 +1,10 @@
 import Image from "next/image";
 import React from "react";
 import fondoteatro from "public/fondoteatro.jpeg";
-import Carrousel from "../Carrousel/Carrousel";
 const Section_one = () => {
   return (
     <section className="flex flex-row bg-gray-300 p-10 h-max justify-evenly items-center">
-      <Carrousel/>
-      
+      Section 2
     </section>
   );
 };
